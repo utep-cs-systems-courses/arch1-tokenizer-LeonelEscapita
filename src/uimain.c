a0a3f78fefd7aabc7d9fd41784d1548c5cb52bde
student <student@systems-vm.lan>
2021-02-10 09:08:47 -0700

    Finished UI and first two methods

---
+++ src/uimain.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+
+int main()
+{
+  char sentence[100];
+  int q;
+  while (1)
+    {
+      printf("enter your string $ ");
+      scanf("%s", sentence);
+      printf(" ");
+
+      
+      printf("quit? 1 for yes, 0 for no ");
+      scanf("%d", &q);
+
+      switch(q){
+      case 1:
+	printf("good-bye!");
+	goto done;
+      case 0:
+	break;
+      default:
+	goto done;
+      }
+        
+	
+    }
+ done:
+  return 0;
+  
+}
