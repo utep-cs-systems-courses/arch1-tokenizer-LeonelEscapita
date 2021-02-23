a20c32744b92eeb0b6bd4edbd30d3cb22e41f7ef
student <student@systems-vm.lan>
2021-02-23 11:46:40 -0700

    generic return statements added

--- src/history.c
+++ src/history.c
@@ -12,8 +12,30 @@ struct s_List {
   struct s_Item *root;
 }
 
+List* init_history()
+{
+  return 0;
+  
+}
+
 void add_history(List *list, char *str)
 {
   printf("history works");
 
+}
+
+char *get_history(List *list, int id)
+{
+  char *p;
+  return p;
+}
+
+void print_history(List *list)
+{
+  printf("History goes here");
+}
+
+void free_history(List *list)
+{
+  free(*list);
 }
