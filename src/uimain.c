a9a34bd1a4037e3d89470bb2097701457899c95f
student <student@systems-vm.lan>
2021-02-26 14:00:40 -0700

    created good ui

--- src/uimain.c
+++ src/uimain.c
@@ -6,29 +6,36 @@
 int main()
 {
   char sentence[100];
-  char q;
+  char c;
+  int id;
+  List *list = init_history();
   while (1)
     {
-      printf("enter your string $");
-      fgets(sentence, 100, stdin);
-      printf("%s\n", sentence); //gets sentence
-
-       //count_words(sentence);
-      char **d = tokenize(sentence); //saves **d with double pointer returned by tokenize
-      print_tokens(d); //prints the list of words that were tokenized
-      free_tokens(d); //frees all allocated memory used for the tokens
-
-      //printf("%s\n",*d);
-      
-      printf("quit? 1 for yes, 0 for no $");
-      q = getchar();
+      printf("Add to History(0), Retrieve from History(1), Quit(2) $");
+      c = getchar();
       getchar(); //catch new line
-      switch (q){
-      case '1':
+      switch (c){
+      case '2':
+	//free_history();
 	printf("good-bye!\n");
 	goto done;
       case '0':
-	
+	printf("enter your string $");
+        fgets(sentence, 100, stdin);
+        printf("%s\n", sentence); //gets string
+        add_history(list, sentence);
+	char **d = tokenize(sentence); //saves **d with double pointer returned by tokenize
+        print_tokens(d); //prints the list of words that were tokenized
+        free_tokens(d); //frees all allocated memory used for the tokens
+	getchar();
+	break;
+      case '1':
+	printf("enter your id $");
+	scanf("%d", &id);
+	//char **r = tokenize(get_history(list, id));
+	//print_tokens(r);
+	//free_tokens(r);
+	getchar();
 	break;
       default:
 	printf("printed something wrong\n");
